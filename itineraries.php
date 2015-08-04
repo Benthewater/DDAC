17819095be318a7d59469d73a5ef1b9a364c1498
Benthewater <ben_tjl@hotmail.com>
2015-08-05 00:20:29 +0800

    Added: homepage.php, itineraries.php, booking.php

---
+++ itineraries.php
@@ -0,0 +1,142 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Itineraries</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="homepage/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="homepage/css/sb-admin.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="homepage/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body>
+
+    <div id="wrapper">
+
+        <!-- Navigation -->
+        <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="index.html">Itineraries</a>
+            </div>
+            <!-- Top Menu Items -->
+            <ul class="nav navbar-right top-nav">
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-user"></i> John Smith <b class="caret"></b></a>
+                    <ul class="dropdown-menu">
+                        <li>
+                            <a href="#"><i class="fa fa-fw fa-user"></i> Profile</a>
+                        </li>
+                        <li>
+                            <a href="#"><i class="fa fa-fw fa-envelope"></i> Inbox</a>
+                        </li>
+                        <li>
+                            <a href="#"><i class="fa fa-fw fa-gear"></i> Settings</a>
+                        </li>
+                        <li class="divider"></li>
+                        <li>
+                            <a href="#"><i class="fa fa-fw fa-power-off"></i> Log Out</a>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+            <!-- Sidebar Menu Items - These collapse to the responsive navigation menu on small screens -->
+            <div class="collapse navbar-collapse navbar-ex1-collapse">
+                <ul class="nav navbar-nav side-nav">
+                    <li>
+                        <a href="homepage.php"><i class="fa fa-fw fa-dashboard"></i> Homepage</a>
+                    </li>
+                    <li class="active">
+                        <a href="itineraries.php"><i class="fa fa-fw fa-bar-chart-o"></i> Itineraries</a>
+                    </li>
+                    <li>
+                        <a href="booking.php"><i class="fa fa-fw fa-table"></i> Booking</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </nav>
+
+        <div id="page-wrapper">
+
+            <div class="container-fluid">
+
+                <!-- Page Heading -->
+                <div class="row">
+                    <div class="col-lg-12">
+                        <h1 class="page-header">
+                            Itineraries
+                        </h1>
+                    </div>
+                </div>
+                <!-- /.row -->
+
+                <!-- Main jumbotron for a primary marketing message or call to action -->
+				<table style="width=100%" class="table">
+					<tr>
+						<td><b>Itinerary Code</b></td>
+						<td><b>Month</b></td>
+						<td><b>Description</b></td>
+						<td><b>Duration</b></td>
+						<td><b>Cost</b></td>
+					</tr>
+					<tr>
+						<td><b>I001</b></td>
+						<td><b>September</b></td>
+						<td><b>Pool Party, Magic Shows</b></td>
+						<td><b>5 days</b></td>
+						<td><b>RM 300.00</b></td>
+					</tr>
+					<tr>
+						<td><b>I002</b></td>
+						<td><b>September</b></td>
+						<td><b>Casino, Children Playground</b></td>
+						<td><b>3 days</b></td>
+						<td><b>RM 250.00</b></td>
+					</tr>
+				</table>
+
+            </div>
+            <!-- /.container-fluid -->
+
+        </div>
+        <!-- /#page-wrapper -->
+
+    </div>
+    <!-- /#wrapper -->
+
+    <!-- jQuery -->
+    <script src="homepage/js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="homepage/js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
